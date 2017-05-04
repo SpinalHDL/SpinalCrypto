5debcd8eafaee9d80974d0ce70395daa154002da
Snoopy <danymorard@hotmail.com>
2017-05-04 21:07:27 +0200

    Reorganize project: Add a specification file for each core

--- src/main/scala/spinalcrypto/hash/md5/MD5Engine_Std.scala
+++ src/main/scala/spinalcrypto/hash/md5/MD5Engine_Std.scala
@@ -215,59 +215,6 @@ case class MD5EngineStdRsp() extends Bundle{
 
 
 /**
-  * MD5 Specification
-  */
-object MD5CoreSpec{
-
-  /** Size of a message block */
-  def msgBlockSize  = 512 bits
-  /** Size of the A B C D block */
-  def subBlockSize  =  32 bits
-  /** Digest message */
-  def hashSize    = 128 bits
-  /** Total number of iterations */
-  def nbrIteration  = 4*16
-
-  def initBlockA = B"x67452301"
-  def initBlockB = B"xEFCDAB89"
-  def initBlockC = B"x98BADCFE"
-  def initBlockD = B"x10325476"
-
-  def funcF(b: Bits, c: Bits, d: Bits): Bits = (b & c) | (~b & d)
-  def funcG(b: Bits, c: Bits, d: Bits): Bits = (b & d) | (~d & c)
-  def funcH(b: Bits, c: Bits, d: Bits): Bits = b ^ c ^ d
-  def funcI(b: Bits, c: Bits, d: Bits): Bits = c ^ (b | ~d)
-
-
-  /** T[i] := floor(2^32 × abs(sin(i + 1))) */
-  def constantT: List[BigInt] = for(i <- List.range(0,64)) yield BigDecimal((pow(2,32) * sin(i + 1.0).abs)).toBigInt()
-
-  /**
-    * ShiftValue is used to know how much left rotation must be done
-    * Original array :
-    *   7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
-    *   5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
-    *   4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
-    *   6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21)
-    */
-  def shiftCstS: List[Int] = List(7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21)
-
-
-  /**
-    * Index K is used to select a word in the 512-bit of the message block
-    *  0 .. 15 : index = i
-    * 16 .. 31 : index = 5 * i + 1 mod 16
-    * 32 .. 47 : index = 3 * i + 5 mod 16
-    * 63 .. 34 : index = 7 * i mod 16
-    */
-  def indexK: List[Int] = for(i <- List.range(0, 64)) yield if      (i < 16) i
-                                                            else if (i < 32) (5 * i + 1) % 16
-                                                            else if (i < 48) (3 * i + 5) % 16
-                                                            else             (7 * i) % 16
-}
-
-
-/**
   * The MD5 algorithm is a hash function producing a 128-bit hash value. MD5 works with block of 512-bit. The message to
   * hash must be padded as following:
   *    - Add a one bit a the end of the message
