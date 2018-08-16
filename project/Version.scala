601e17d90c2f07afccead28ed575eef4214bb6fc
Morard Dany <danymorard@hotmail.com>
2018-08-16 13:13:28 +0200

    Add a valid version

--- project/Version.scala
+++ project/Version.scala
@@ -1,8 +1,8 @@
 object SpinalVersion {
   val compiler = "2.11.6"
     
-  private val major = "0.1.0"
-  
-  val tester   = s"$major"
-  val crypto   = s"$major"
+  private val version = "1.0.0"
+
+  val tester   = s"$version"
+  val crypto   = s"$version"
 }
