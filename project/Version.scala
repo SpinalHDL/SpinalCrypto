a14bba200f53120a5a8bff263687abda1170ee4e
Morard Dany <dany.morard@nagra.com>
2019-07-09 07:05:51 +0200

    Update version

--- project/Version.scala
+++ project/Version.scala
@@ -1,7 +1,7 @@
 object SpinalVersion {
   val compiler = "2.11.6"
     
-  private val version = "1.0.1"
+  private val version = "1.1.0"
 
   val tester   = s"$version"
   val crypto   = s"$version"
