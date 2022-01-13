18963b64783c52f4d2c45d066c79aebff165517f
Dolu1990 <charles.papon.90@gmail.com>
2022-01-13 17:03:34 +0100

    SpinalHDL 1.6.1

--- project/Version.scala
+++ project/Version.scala
@@ -1,7 +1,7 @@
 object CryptoVersion {
 
   val scalaCompilers = Seq("2.11.12", "2.12.13", "2.13.6")
-  val spinal        = "1.6.0"
+  val spinal        = "1.6.1"
 
   private val version = "1.1.1"
   val tester   = s"$version"
